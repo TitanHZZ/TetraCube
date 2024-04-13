2713becbdb9183de39ef8866663bece5342948a6
TitanHZZ <titanhzz2018@gmail.com>
2024-04-13 19:09:15 +0100

    Made the camera look directly into the game grid by default.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { DoubleSide } from "three";
 import Grid from "../components/Grid";
-import Piece, { PieceTypes } from "../components/Piece";
+import { useEffect, useRef } from "react";
 
-function App() {
+function App() {    
+    const orbitRef = useRef(null);
+    
+    useEffect(() => {
+        if (orbitRef.current) {
+            const distance = 20 / 2 / Math.tan((Math.PI * 30) / 360);
+
+            // change the position
+            orbitRef.current.object.position.set(distance / 2, distance / 2, distance / 2);
+
+            // change the camera target (where the camera is looking at)
+            orbitRef.current.target.set(0, 6, 0);
+        }
+    }, [orbitRef.current]);
+
     return (
-        <div className="h-screen" style={{ backgroundColor: "#484954" }}>
-            <Canvas camera={{
-                position: [0, 0, 7],
-                fov: 30
-            }}>
-                <OrbitControls />
-                <ambientLight intensity={0.8} />
-                <Grid size={6} />
-
-                <Piece type={PieceTypes.OrangeRicky} position={[0, 12, 0]} />
-
-                <mesh>
-                    <planeGeometry args={[20, 20]} />
-                    <meshBasicMaterial color="grey" side={DoubleSide} visible={false} />
-                </mesh>
-            </Canvas>
-        </div>
+        <>
+            <OrbitControls ref={orbitRef} />
+            <ambientLight intensity={0.8} />
+            <Grid size={6} />
+        </>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { Canvas } from '@react-three/fiber';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <div className="h-screen" style={{ backgroundColor: "#484954" }}>
+        <Canvas camera={{ fov: 30 }}>
+            <App />
+        </ Canvas >
+    </div>
 );
