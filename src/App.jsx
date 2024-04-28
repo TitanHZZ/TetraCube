1174a768ccd39590d7a336e1b1f8d3bddefc8670
TitanHZZ <titanhzz2018@gmail.com>
2024-04-28 10:35:37 +0100

    Sounds.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
     const [score, setScore] = useState(0);
     const startButtonRef = useRef(null);
     const pauseButtonRef = useRef(null);
+    const ambientAudioRef = useRef(null);
+    const pointsAudioRef = useRef(null);
 
     return (
         <div className="h-screen overflow-hidden" style={{ backgroundColor: "#484954" }}>
@@ -19,9 +21,15 @@ function App() {
                 <button className={`${gameState !== GameState.Quit ? "bg-red-500 hover:bg-red-700" : "bg-green-500 hover:bg-green-700"} w-20 text-white font-bold my-4 ml-5 px-5 rounded`} onClick={() => {
                     if (gameState === GameState.Quit) {
                         setGameState(GameState.Ongoing);
+                        ambientAudioRef.current.volume = 1;
+                        ambientAudioRef.current.play();
                     } else {
                         setGameState(GameState.Quit);
                         setScore(0);
+
+                        ambientAudioRef.current.pause();
+                        ambientAudioRef.current.volume = 0;
+                        ambientAudioRef.current.currentTime = 0;
                     }
 
                     startButtonRef.current.blur();
@@ -39,7 +47,10 @@ function App() {
                 <div className="flex justify-end">
                     <div style={{ width: '40rem' }}>
                         <Canvas camera={{ fov: 30 }} shadows>
-                            <Game state={gameState} onPoints={(points) => setScore(score + points)} onGameOver={() => setGameState(GameState.GameOver)} onNewPiece={(newPieceType) => setNextPieceType(newPieceType)} />
+                            <Game state={gameState} onPoints={(points) => {
+                                setScore(score + points);
+                                pointsAudioRef.current.play();
+                            }} onGameOver={() => setGameState(GameState.GameOver)} onNewPiece={(newPieceType) => setNextPieceType(newPieceType)} />
                         </ Canvas >
                     </div>
                 </div>
@@ -69,6 +80,12 @@ function App() {
             <div className={`absolute top-1/2 right-1/2 bg-white px-5 py-4 font-bold text-2xl rounded ${gameState === GameState.GameOver ? 'visible' : 'invisible'}`} style={{ transform: 'translate(50%, -50%)' }}>
                 Game over
             </div>
+            <audio ref={ambientAudioRef} loop>
+                <source src="ambient_audio.mp3" type="audio/mp3" />
+            </audio>
+            <audio ref={pointsAudioRef}>
+                <source src="points.mp3" type="audio/mp3" />
+            </audio>
         </div>
     );
 }
